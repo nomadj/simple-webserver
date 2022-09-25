696954ea3756e40cd85c00719a8b0269b4d66899
Bryan Albert <bryan2j@gmail.com>
2022-09-24 23:27:55 -0600

    Add notIndex.html and write fileReader function

--- server.js
+++ server.js
@@ -7,38 +7,14 @@ const port = 8080;
 
 var server = http.createServer(function(request, response) {
   var path = url.parse(request.url).pathname;
+  
   switch (path) {
     case '/':
-      fs.readFile(__dirname + '/index.html', function(error, data) {
-	if (error) {
-	  response.writeHead(404);
-	  response.write(error);
-	  response.end();
-	} else {
-	  response.writeHead(200, {
-	    'Content-Type': 'text/html'
-	  });
-	  response.write(data);
-	  response.end();
-	}
-      });
+      fileReader('/index.html');
       break;
-
-    // Replace string below to load pages not named 'index.html'
+    // Edit string below appropriately to load pages not named 'index.html'
     case '/notIndex.html':
-      fs.readFile(__dirname + path, function(error, data) {
-	if (error) {
-	  response.writeHead(404);
-	  response.write(error);
-	  response.end(error);
-	} else {
-	  response.writeHead(200, {
-	    'Content-Type': 'text/html'
-	  });
-	  response.write(data);
-	  response.end();
-	}
-      });
+      fileReader(path);
       break;
     default:
       response.writeHead(404);
@@ -46,6 +22,22 @@ var server = http.createServer(function(request, response) {
       response.end();
       break;
   }
+  
+  function fileReader(path) {
+    fs.readFile(__dirname + path, function(error, data) {
+      if (error) {
+	response.writeHead(404);
+	response.write(error);
+	response.end(error);
+      } else {
+	response.writeHead(200, {
+	  'Content-Type': 'text/html'
+	});
+	response.write(data);
+	response.end();
+      }
+    });
+  }  
 });
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
