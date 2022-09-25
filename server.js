a9e5a0d39f507999ba56201ec3054225fe3ed155
Bryan Albert <bryan2j@gmail.com>
2022-09-24 23:53:25 -0600

    Destructuring assignment to createServer

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var http = require("http");
+var { createServer } = require("http");
 var url = require("url");
 var fs = require("fs");
 
 const host = 'localhost';
 const port = 8080;
 
-var server = http.createServer(function(request, response) {
+var server = createServer(function(request, response) {
   var path = url.parse(request.url).pathname;
   
   switch (path) {
