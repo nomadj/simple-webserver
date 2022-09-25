60104b8a72ea790bf3b60fc9200b51d6c510f6e8
Bryan Albert <bryan2j@gmail.com>
2022-09-25 01:40:59 -0600

    Use express server in order to load css

---
+++ http-server.js
@@ -0,0 +1,44 @@
+var { createServer } = require("http");
+var url = require("url");
+var { readFile } = require("fs");
+
+const host = 'localhost';
+const port = 8080;
+
+var server = createServer(function(request, response) {
+  var path = url.parse(request.url).pathname;
+  
+  switch (path) {
+    case '/':
+      fileReader('/index.html');
+      break;
+    // Edit string below appropriately to load pages not named 'index.html'
+    case '/notIndex.html':
+      fileReader(path);
+      break;
+    default:
+      response.writeHead(404);
+      response.write("This page does not exist - 404");
+      response.end();
+      break;
+  }
+  
+  function fileReader(path) {
+    readFile(__dirname + path, function(error, data) {
+      if (error) {
+	response.writeHead(404);
+	response.write(error);
+	response.end(error);
+      } else {
+	response.writeHead(200, {
+	  'Content-Type': 'text/html'
+	});
+	response.write(data);
+	response.end();
+      }
+    });
+  }  
+});
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
--- server.js
+++ server.js
@@ -1,44 +1,12 @@
-var { createServer } = require("http");
-var url = require("url");
-var fs = require("fs");
+const express = require("express");
+const url = require("url");
+const app = express();
+app.use(express.static("public"));
 
-const host = 'localhost';
-const port = 8080;
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
 
-var server = createServer(function(request, response) {
-  var path = url.parse(request.url).pathname;
-  
-  switch (path) {
-    case '/':
-      fileReader('/index.html');
-      break;
-    // Edit string below appropriately to load pages not named 'index.html'
-    case '/notIndex.html':
-      fileReader(path);
-      break;
-    default:
-      response.writeHead(404);
-      response.write("This page does not exist - 404");
-      response.end();
-      break;
-  }
-  
-  function fileReader(path) {
-    fs.readFile(__dirname + path, function(error, data) {
-      if (error) {
-	response.writeHead(404);
-	response.write(error);
-	response.end(error);
-      } else {
-	response.writeHead(200, {
-	  'Content-Type': 'text/html'
-	});
-	response.write(data);
-	response.end();
-      }
-    });
-  }  
-});
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+app.listen(8080, function() {
+  console.log("Listening on port 8080.");
 });
